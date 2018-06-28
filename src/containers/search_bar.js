26a2e115d3fce255e4f37a59fcdab0f7f7697025
Sam <sammintz1993@gmail.com>
2018-06-28 17:18:02 -0500

    handleSubmit for form

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -5,15 +5,24 @@ export default class SearchBar extends Component {
 		super(props)
 
 		this.state = { term : ''}
+
+		// need to bind the context of this so it is correct context.
+		// also seems to work if you just use arrow function for onInputChange()
+		// this.OnonInputChange = this.onInputChange.bind(this)
 	}
 
 	onInputChange = (e) => {
 		console.log(e.target.value)
+		this.setState({ term: e.target.value})
+	}
+
+	handleSubmit = (e) => {
+		e.preventDefault()
 	}
 
 	render(){
 		return (
-			<form className="input-group">
+			<form onSubmit={this.handleSubmit} className="input-group">
 				<input 
 				placeholder="Get a five-day forecast in your favorite cities"
 				className="form-control"
