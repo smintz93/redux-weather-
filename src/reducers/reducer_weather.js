053f30531d0a04516f02ae4caff80ca311a8c9f0
Sam <sammintz1993@gmail.com>
2018-06-29 09:32:50 -0500

    weather listin app.js

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,3 +1,14 @@
-export default function(state = null, action){
+import { FETCH_WEATHER } from '../actions/index';
+
+// need an array bc need multiple cities at once to be displayed. 
+
+export default function(state = [], action){
+	switch (action.type){
+	case FETCH_WEATHER:
+	// concat makes a new array
+	// cant mutate state
+		// return state.concat([action.payload.data]);
+		return [ action.payload.data, ...state ]; // [ city, city, city]
+	}
 	return state;
 }
