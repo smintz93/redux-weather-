35e6a8dd0bf3dbe7c158c2d24dd87a250361e0f3
Sam <sammintz1993@gmail.com>
2018-07-09 14:40:13 -0500

    when searching for city, google map city picture appears

--- src/components/google_map.js
+++ src/components/google_map.js
@@ -1,7 +1,23 @@
 import React, { Component } from 'react';
 
 class GoogleMap extends Component {
+	componentDidMount() {
+		// embed google maps inside docuemtn 
+		new google.maps.Map(this.refs.map, {
+			zoom: 12,
+			center: {
+				lat: this.props.lat,
+				lng: this.props.lon
+			}
+		});
+	}
 
+
+	render() {
+		// ref allows us to reference html element that has been rendered to map
+		// this.refs.map
+		return <div ref="map" />;
+	}
 
 }
 
